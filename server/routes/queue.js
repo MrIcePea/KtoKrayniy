1c3b0371d8b3a89022ea8d7b07b6492784cf7366
Ilya <mrisp007@gmail.com>
2022-06-17 18:16:20 +0300

    better queue back

--- server/routes/queue.js
+++ server/routes/queue.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Queue } = require('../db/models');
+const { Queue, User } = require('../db/models');
 
 router.get('/getqueue', async (req, res) => {
   const queue = await Queue.findAll(
@@ -7,6 +7,14 @@ router.get('/getqueue', async (req, res) => {
       order: [
         ['id', 'ASC'],
       ],
+      include: {
+        model: User,
+        where: { role: 'user' },
+        attributes: { exclude: ['role', 'pass'] },
+        order: [
+          ['rank', 'DESC'],
+        ],
+      },
     },
   );
   console.log(queue);
