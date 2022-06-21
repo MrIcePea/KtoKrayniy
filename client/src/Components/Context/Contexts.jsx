37a84d0b6f27596863e24d1f17f55234b7154eae
Vasily <89163091443@mail.ru>
2022-06-21 20:27:01 +0300

    socket + .env

--- client/src/Components/Context/Contexts.jsx
+++ client/src/Components/Context/Contexts.jsx
@@ -6,17 +6,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Queue } from '../../Redux/Actions/getQueueAction';
 
 const TodoContext = createContext();
+console.log(process.env.REACT_APP_WS_URL);
 
 export default function TodoContextProvider({ children }) {
   // const { queue, user } = useSelector((state) => state);
   const { id } = useSelector((state) => state.user);
-  const [socket, setSocket] = useState(new WebSocket('ws://192.168.1.98:3001'));
+  const [socket, setSocket] = useState(new WebSocket(process.env.REACT_APP_WS_URL));
   console.log('1---socket---TodoContextProvider---', socket);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (id) {
-      setSocket(new WebSocket('ws://192.168.1.98:3001'));
+      setSocket(new WebSocket(process.env.REACT_APP_WS_URL));
       console.log('2--------------------');
     }
   }, [id]);
